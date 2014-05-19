9688707d00fc5def022646b306db24a9c6de44df
Stefan van der Walt <stefan@sun.ac.za>
2014-05-19 16:34:50 +0200

    Add matplotlib waveform plotting

--- bytebeats.py
+++ bytebeats.py
@@ -3,6 +3,7 @@
 # ./bytebeats.py | aplay
 
 import sys
+import numpy as np
 
 tunes = [
     # http://royal-paw.com/2012/01/bytebeats-in-c-and-python-generative-symphonies-from-extremely-small-programs/
@@ -24,12 +25,45 @@ tunes = [
     # @isislovecruft
     lambda t: (~t>>2)*(2+(42&t*((7&t>>10)*2))<(24&t*((3&t>>14)+2))),
     lambda t: (t*5&t>>7|t*9&t>>4|t*18&t/1024)|((t|7)>>5|(t|4)>>9),
-    lambda t: ((t*(t>>13|t>>8))|(t>>16)^t)-64
+    lambda t: ((t*(t>>13|t>>8))|(t>>16)^t)-64,
+
+    # Ville-Matias Heikkila
+    lambda t: (t*5&t>>7)|(t*3&t>>10),
+
+    # viznut, http://wry.me/bytebeat/examples.html
+    lambda t: (t&t%255)-(t*3&t>>13&t>>6),
     ]
 
-def play(f):
-    for t in xrange(2**19):
-        sys.stdout.write(chr(f(t) % 256))
+def play(song):
+    import matplotlib.pyplot as plt
+    from matplotlib import animation
+
+    bufsize = 300
+
+    fig, ax = plt.subplots()
+
+    line, = plt.plot(np.zeros((bufsize,)))
+    ax.set_ylim([0, 255])
+
+    frames = xrange(2**30)
+
+    def play_samples(t, line, song, bufsize):
+        # Load more data into the sound buffer than what is displayed
+        t0 = np.arange(t * bufsize, (t + 1) * bufsize, dtype=np.int64)
+        t1 = np.arange(t * (bufsize * 1.5), (t + 1) * (bufsize * 1.5), dtype=np.int64)
+        samples0 = song(t0) % 256
+        samples1 = song(t1) % 256
+        for s in samples1:
+            sys.stdout.write(chr(s))
+        #sys.stdout.write(samples.tostring())
+        #sys.stdout.write(samples)
+        line.set_ydata(samples0)
+        return line,
+
+    ani = animation.FuncAnimation(fig, func=play_samples, frames=frames,
+                                  fargs=(line, song, bufsize),
+                                  interval=float(bufsize)/8000 * 1000, blit=False)
+    plt.show()
 
 try:
     n = int(sys.argv[1])
@@ -40,9 +74,9 @@ except:
     print "There are %d songs." % len(tunes)
     sys.exit(0)
 
-if sys.stdout.isatty():
-        print "Stdout goes to terminal--you probably don't want to do that."
-        print "Pipe stdout to `aplay` instead."
-        sys.exit(0)
+## if sys.stdout.isatty():
+##         print "Stdout goes to terminal--you probably don't want to do that."
+##         print "Pipe stdout to `aplay` instead."
+##         sys.exit(0)
 
 play(tunes[n])
